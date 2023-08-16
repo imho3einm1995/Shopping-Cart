621f4cb6e1556457bac46ef2152385ac34996187
imho3einm1995 <122366657+imho3einm1995@users.noreply.github.com>
2023-08-16 11:50:48 +0330

    initial commit

---
+++ src/context.jsx
@@ -0,0 +1,40 @@
+import { useReducer } from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { createContext } from "react";
+import CartItems from "./data"
+import reducer from "./reducer";
+
+const CartContext = createContext();
+
+const initialState = {loaging:false, cart:CartItems, total:0, amount:0};
+
+const CartProvider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const clearCart = () => {
+        dispatch({type: 'CLEAR_CART'});
+    }
+
+    const remove = id => {
+        dispatch({type: 'REMOVE', payload: id});
+    }
+
+    const changeQuantity = changeObj => {
+        dispatch({type: 'CHANGE_QUANTITY', payload : changeObj})
+    }
+
+    useEffect(() => {
+        dispatch({type:'GET_TOTALS'});
+    }, [state.cart])
+
+    return (
+        <CartContext.Provider value={{...state, clearCart, remove, changeQuantity}}>{children}</CartContext.Provider>
+    )
+}
+
+const useCartContext = () => {
+    return useContext(CartContext);
+};
+
+export {CartProvider, useCartContext};
